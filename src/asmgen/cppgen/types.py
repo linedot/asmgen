7b259178d93f520775a0535ea9c760557deb0f9c
linedot <linedot@linedot.org>
2025-04-09 07:37:18 +0200

    Add more c_data_types

--- src/asmgen/cppgen/types.py
+++ src/asmgen/cppgen/types.py
@@ -4,6 +4,17 @@ C++ data type names
 from asmgen.asmblocks.noarch import asm_data_type
 
 c_data_types = {
-        asm_data_type.DOUBLE : "double",
-        asm_data_type.SINGLE : "float",
+        asm_data_type.DOUBLE   : "double",
+        asm_data_type.UINT64   : "uint64_t",
+        asm_data_type.SINT64   : "int64_t",
+        asm_data_type.SINGLE   : "float",
+        asm_data_type.UINT32   : "uint32_t",
+        asm_data_type.SINT32   : "int32_t",
+        asm_data_type.HALF     : "uint16_t",
+        asm_data_type.UINT16   : "uint16_t",
+        asm_data_type.SINT16   : "int16_t",
+        asm_data_type.FP8E4M3  : "uint8_t",
+        asm_data_type.FP8E5M2  : "uint8_t",
+        asm_data_type.UINT8    : "uint8_t",
+        asm_data_type.SINT8    : "int8_t"
         }
