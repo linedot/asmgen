9581f1abc38bb78b3a1980910b80666e881f6463
linedot <linedot@linedot.org>
2025-06-13 13:35:36 +0200

    fix rvv opd3 fma tests operand order

--- tests/opd3/rvv/test_fmla.py
+++ tests/opd3/rvv/test_fmla.py
@@ -25,32 +25,32 @@ class test_rvv_opd3(unittest.TestCase):
         gen.set_output_inline(yesno=False)
 
         self.assertEqual(
-            "vfmacc.vv v0,v1,v2\n",
+            "vfmacc.vv v0,v2,v1\n",
             gen.fma(adreg=gen.vreg(1),bdreg=gen.vreg(2),cdreg=gen.vreg(0),
                     a_dt=adt.FP64, b_dt=adt.FP64, c_dt=adt.FP64))
 
         self.assertEqual(
-            "vfmacc.vv v0,v1,v2\n",
+            "vfmacc.vv v0,v2,v1\n",
             gen.fma(adreg=gen.vreg(1),bdreg=gen.vreg(2),cdreg=gen.vreg(0),
                     a_dt=adt.FP32, b_dt=adt.FP32, c_dt=adt.FP32))
 
         self.assertEqual(
-            "vfmacc.vv v0,v1,v2\n",
+            "vfmacc.vv v0,v2,v1\n",
             gen.fma(adreg=gen.vreg(1),bdreg=gen.vreg(2),cdreg=gen.vreg(0),
                     a_dt=adt.FP16, b_dt=adt.FP16, c_dt=adt.FP16))
 
         self.assertEqual(
-            "vfwmacc.vv v0,v2,v3\n",
+            "vfwmacc.vv v0,v3,v2\n",
             gen.fma(adreg=gen.vreg(2),bdreg=gen.vreg(3),cdreg=gen.vreg(0),
                     a_dt=adt.FP16, b_dt=adt.FP16, c_dt=adt.FP32))
 
         self.assertEqual(
-            "vmacc.vv v0,v1,v2\n",
+            "vmacc.vv v0,v2,v1\n",
             gen.fma(adreg=gen.vreg(1),bdreg=gen.vreg(2),cdreg=gen.vreg(0),
                     a_dt=adt.SINT32, b_dt=adt.SINT32, c_dt=adt.SINT32))
 
         self.assertEqual(
-            "vfwnmsac.vf v0,v2,f0\n",
+            "vfwnmsac.vf v0,f0,v2\n",
             gen.fma(adreg=gen.vreg(2),bdreg=gen.freg(0,adt.FP16),cdreg=gen.vreg(0),
                     a_dt=adt.FP16, b_dt=adt.FP16, c_dt=adt.FP32,
                     modifiers={mod.NP,mod.VF}))
