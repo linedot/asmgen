533fb5b613d336698bfee903778aa0991c6c3732
linedot <linedot@linedot.org>
2025-06-13 13:35:03 +0200

    Specify treg datatype in SME opd3 tests

--- tests/opd3/sme/test_fopa.py
+++ tests/opd3/sme/test_fopa.py
@@ -25,40 +25,40 @@ class test_sme_opd3(unittest.TestCase):
         gen.set_output_inline(yesno=False)
         self.assertEqual(
             "fmopa za0.d,p0/m,p0/m,z0.d,z1.d\n",
-            gen.fopa(adreg=gen.vreg(0),bdreg=gen.vreg(1),cdreg=gen.treg(0),
+            gen.fopa(adreg=gen.vreg(0),bdreg=gen.vreg(1),cdreg=gen.treg(0, adt.FP64),
                 a_dt=adt.FP64, b_dt=adt.FP64, c_dt=adt.FP64))
         self.assertEqual(
             "fmops za0.d,p0/m,p0/m,z0.d,z1.d\n",
-            gen.fopa(adreg=gen.vreg(0),bdreg=gen.vreg(1),cdreg=gen.treg(0),
+            gen.fopa(adreg=gen.vreg(0),bdreg=gen.vreg(1),cdreg=gen.treg(0, adt.FP64),
                 a_dt=adt.FP64, b_dt=adt.FP64, c_dt=adt.FP64,
                      modifiers={mod.NP}))
         self.assertEqual(
             "fmopa za0.s,p0/m,p0/m,z0.s,z1.s\n",
-            gen.fopa(adreg=gen.vreg(0),bdreg=gen.vreg(1),cdreg=gen.treg(0),
+            gen.fopa(adreg=gen.vreg(0),bdreg=gen.vreg(1),cdreg=gen.treg(0, adt.FP32),
                 a_dt=adt.FP32, b_dt=adt.FP32, c_dt=adt.FP32))
         self.assertEqual(
             "fmops za0.s,p0/m,p0/m,z0.s,z1.s\n",
-            gen.fopa(adreg=gen.vreg(0),bdreg=gen.vreg(1),cdreg=gen.treg(0),
+            gen.fopa(adreg=gen.vreg(0),bdreg=gen.vreg(1),cdreg=gen.treg(0, adt.FP32),
                 a_dt=adt.FP32, b_dt=adt.FP32, c_dt=adt.FP32,
                      modifiers={mod.NP}))
         self.assertEqual(
             "fmopa za0.h,p0/m,p0/m,z0.h,z1.h\n",
-            gen.fopa(adreg=gen.vreg(0),bdreg=gen.vreg(1),cdreg=gen.treg(0),
+            gen.fopa(adreg=gen.vreg(0),bdreg=gen.vreg(1),cdreg=gen.treg(0, adt.FP16),
                 a_dt=adt.FP16, b_dt=adt.FP16, c_dt=adt.FP16))
         self.assertEqual(
             "fmops za0.h,p0/m,p0/m,z0.h,z1.h\n",
-            gen.fopa(adreg=gen.vreg(0),bdreg=gen.vreg(1),cdreg=gen.treg(0),
+            gen.fopa(adreg=gen.vreg(0),bdreg=gen.vreg(1),cdreg=gen.treg(0, adt.FP16),
                 a_dt=adt.FP16, b_dt=adt.FP16, c_dt=adt.FP16,
                      modifiers={mod.NP}))
 
         with self.assertRaises(ValueError):
-            gen.fopa(adreg=gen.vreg(0),bdreg=gen.vreg(1),cdreg=gen.treg(0),
+            gen.fopa(adreg=gen.vreg(0),bdreg=gen.vreg(1),cdreg=gen.treg(0, adt.FP64),
                 a_dt=adt.FP16, b_dt=adt.FP32, c_dt=adt.FP64)
 
         with self.assertRaises(ValueError):
-            gen.fopa(adreg=gen.vreg(0),bdreg=gen.vreg(1),cdreg=gen.treg(0),
+            gen.fopa(adreg=gen.vreg(0),bdreg=gen.vreg(1),cdreg=gen.treg(0, adt.UINT64),
                 a_dt=adt.UINT64, b_dt=adt.UINT64, c_dt=adt.UINT64)
 
         with self.assertRaises(ValueError):
-            gen.fopa(adreg=gen.vreg(0),bdreg=gen.vreg(1),cdreg=gen.treg(0),
+            gen.fopa(adreg=gen.vreg(0),bdreg=gen.vreg(1),cdreg=gen.treg(0, adt.UINT64),
                 a_dt=adt.FP32, b_dt=adt.FP32, c_dt=adt.UINT64)
